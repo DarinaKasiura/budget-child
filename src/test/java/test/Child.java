d7409bf1e54acdd6af00beb1ed0388bc28529e60
DarinaKasiura <darina.kasyura95@gmail.com>
2018-12-01 18:57:33 -0600

    first commit

---
+++ src/test/java/test/Child.java
@@ -0,0 +1,14 @@
+package test;
+
+import utilities.BrowserUtils;
+import beans.Country;
+
+public class Child {
+
+	public static void main(String[] args) {
+		//BrowserUtils.scrollDownToElement();
+		
+
+	}
+
+}
